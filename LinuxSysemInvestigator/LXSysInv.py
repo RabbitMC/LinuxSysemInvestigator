557e7b68f35610563ac92fbff3fee1714669a1cc
Miralem Cebic <mail@miralem-cebic.de>
2016-02-20 08:34:09 +0100

    started

---
+++ LinuxSysemInvestigator/LXSysInv.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import time
+
+
+class LXSysInv:
+    """Linux System Inventigator main class"""
+    def __init__(self):
+        """ init method """
+        self.data = []
