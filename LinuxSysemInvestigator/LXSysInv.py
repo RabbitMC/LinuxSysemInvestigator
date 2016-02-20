b08ab1f9423a9e0a2a0b2c6f2a5a7e63357c260d
Miralem Cebic <mail@miralem-cebic.de>
2016-02-20 10:17:39 +0100

    --

--- LinuxSysemInvestigator/LXSysInv.py
+++ LinuxSysemInvestigator/LXSysInv.py
@@ -1,10 +1,35 @@
 import sys
 import os
 import time
-
+import json
 
 class LXSysInv:
     """Linux System Inventigator main class"""
     def __init__(self):
         """ init method """
         self.data = []
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+            sort_keys=True, indent=4)
+
+
+
+
+
+unumber = os.getuid()
+pnumber = os.getpid()
+where = os.getcwd()
+what = os.uname()
+used = os.times()
+now = time.time()
+means = time.ctime(now)
+
+print "User number",unumber
+print "Process ID",pnumber
+print "Current Directory",where
+print "System information",what
+print "System information",used
+
+print "\nTime is now",now
+print "Which interprets as",means
--- LinuxSysemInvestigator/__init__.py
+++ LinuxSysemInvestigator/__init__.py
@@ -3,9 +3,10 @@ from os import path
 import LXSysInv
 
 
-parser = argparse.ArgumentParser(description='LinuxSystemInvestigator')
-parser.add_argument('-o', dest="output path dir, where to store the resulting json file.")
-args = parser.parse_args()
+#parser = argparse.ArgumentParser(description='LinuxSystemInvestigator')
+#parser.add_argument('-o', dest="output path dir, where to store the resulting json file.")
+#args = parser.parse_args()
 
-if path.exists(args.o):
-    linuxSysInv = LXSysInv()
+#if path.exists(args.a):
+
+linuxSysInv = LXSysInv()
